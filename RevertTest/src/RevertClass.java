0aae41b5a48c04d7da181d058ea85ef891ddfb48
anis-mzt <anis.mzt@gmail.com>
2016-04-11 11:13:30 +0100

    Update RevertClass.java

--- RevertTest/src/RevertClass.java
+++ RevertTest/src/RevertClass.java
@@ -4,6 +4,7 @@ public class RevertClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Try to Revert !");
+System.out.println("Try to Revert 2 !");
 	}
 
 }
